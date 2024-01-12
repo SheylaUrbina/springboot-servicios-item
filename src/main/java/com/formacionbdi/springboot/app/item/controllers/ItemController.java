977b60117067409c16c02d4d0314edadde8b2214
surbina <sheyla.ulopez@gmail.com>
2024-01-11 19:45:41 -0600

    Actualizando servicio items despues de meses

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,17 +1,29 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
@@ -22,15 +34,20 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 //import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
-
-
+//permite actualizar controladores, componentes, clases anotadas con component, con service, inyectando con value, environment
+@RefreshScope 
 @RestController
 public class ItemController {
 	
 	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@Autowired
 	@Qualifier("serviceFeign") //aqui le indicmos cual service usara si el resttemplate o el feign
@@ -38,6 +55,39 @@ public class ItemController {
 	//@Qualifier("serviceRestTemplate") //Qualifier, sirven para determinar el componente rest a utilizar y principal
 	private ItemService itemService;
 
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+		return itemService.save(producto);
+	}
+	
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto,@PathVariable Long id) {
+		return itemService.update(producto,id);
+	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		itemService.delete(id);
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}")String puerto){
+		logger.info(texto);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String,String>>(json, HttpStatus.OK);
+	}
+	
 	@GetMapping("/listar")
 	public List<Item> listar(@RequestParam(name="nombre", required= false) String nombre, @RequestHeader(name="token-request", required= false) String token){
 		System.out.println("nombre: "+nombre);
